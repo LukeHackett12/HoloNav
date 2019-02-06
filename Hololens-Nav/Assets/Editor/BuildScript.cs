9454f43aa12ea23ed8d8e1b89b5df97284d7674a
Luke Hackett <lhackett@tcd.ie>
2019-02-06 17:03:32 +0000

    Buildscript Added

---
+++ Hololens-Nav/Assets/Editor/BuildScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+public class BuildScript
+{
+    static void build()
+    {
+        // Place all your scenes here
+        string[] scenes = {"Assets/scenes/SampleScene.unity"};
+
+        string pathToDeploy = "UWP/";
+
+        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.WSAPlayer, BuildOptions.Development);
+    }
+}
