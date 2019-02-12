724393c6438a58602809ef9a990c4f95bcf5afea
Luke Hackett <lhackett@tcd.ie>
2019-02-12 13:01:17 +0000

    Added s3 deploy script

---
+++ deployS3.ps1
@@ -0,0 +1,5 @@
+﻿$x="BRANCH_NAME";
+$y = (get-item env:$x).Value;
+Rename-Item "Hololens-nav/UWP/build/bin/Win32" "Win32-$y";
+echo $y;
+aws s3 cp "Hololens-nav/UWP/build/bin/" s3://hololens-nav --recursive --include "*";
