5aa215479f5a45cc63a4b0f976b456520fe5968b
Imalia Gonzalezova <46975468+imgo7@users.noreply.github.com>
2019-02-27 20:38:18 +0000

    Exit button, search bar and dropdown.
    Added scripts to the exit button and search bar. Added a dropdown options feature.

---
+++ Hololens-Nav/Assets/getDest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //for input field
+
+public class getDest : MonoBehaviour
+{
+    public InputField location; //user's desired location
+    public Text locText; //location in text form
+
+   public void getDestination()
+    {
+        locText.text = location.text;
+    }
+}
