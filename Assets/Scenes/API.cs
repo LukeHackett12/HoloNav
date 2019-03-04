d02f4fe429e640496011fc41376cc986313ef481
Jamison Engels <40210291+engelsj@users.noreply.github.com>
2019-03-04 11:57:41 +0000

    Add files via upload
    Small nav app using mapbox navigation API

---
+++ Assets/Scenes/API.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class API : MonoBehaviour
+{
+    private const string URL = "https://api.mapbox.com/directions/v5/mapbox/walking/";
+    private const string API_KEY = "pk.eyJ1IjoiamVuZ2VsczAzIiwiYSI6ImNqaXZ1aDVjODE0ZDQzam56dHJhc3dmNG8ifQ.2jBiS1t1o8uY4KuSbBMOtg";
+    private string destination;
+    public Text responseText;
+
+    public void request()
+    {
+
+        WWW request = new WWW(URL + destination+ "?access_token=" + API_KEY);
+        StartCoroutine(OnResponse(request));
+    }
+
+    private IEnumerator OnResponse(WWW req)
+    {
+        yield return req;
+
+        responseText.text = req.text;
+    }
+
+    public void Text_Changed(string newText)
+    {
+        destination = newText;
+    }
+}
