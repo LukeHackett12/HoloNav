8e06b96a6c6942a975e338438ac0ebe0f367698b
imgo7 <46975468+imgo7@users.noreply.github.com>
2019-03-09 19:30:00 +0000

    Changes to Menu
    Changes in the settings- so menu may be viewed in Hololens

---
+++ Hololens-Nav/Assets/RouteDest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RouteDest : MonoBehaviour
+{
+    public Dropdown dropdown; //reference to dropdown menu
+    public Text selectedRoute; //feedback which route chosen
+
+    List<string> routes = new List<string> { "Route to Destination","Microsoft to Trinity",
+        "Trinity to Tymon Park", "Tymon Park to Microsoft"}; //global list
+
+    public void changeRoute(int index) //route selected
+    {
+        if(index == 0)
+        {
+            selectedRoute.color = Color.red; //red ? mark
+            selectedRoute.text = "?"; //no route selected
+        }
+        //Microsoft to Trinity index 1
+        if(index == 1)
+        {
+            
+            selectedRoute.text = "1"; //Route 1
+        }
+        //Trinity to Tymon Park 2
+        if (index == 2)
+        {
+            selectedRoute.text = "2"; //Route 2
+        }
+        //Tymon Park to Microsoft 3
+        if (index == 3)
+        {
+            selectedRoute.text = "3"; //Route 3
+        }
+        // selectedRoute.text = routes[index]; //gives the route selected
+    }
+
+    void Start()
+    {
+        getRoutesToDest(); //get list of destinations
+    }
+   
+    void getRoutesToDest()
+    { 
+        dropdown.AddOptions(routes);
+
+    }
+}
---
+++ Hololens-Nav/Assets/getDest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //for input field
+
+public class getDest : MonoBehaviour
+{
+    public InputField location; //user's desired location
+    public Text locText; //location in text form
+
+   public void getDestination()
+    {
+        locText.text = location.text;
+        Debug.Log("clicked" + locText.text);
+        //return option selected
+    }
+}
