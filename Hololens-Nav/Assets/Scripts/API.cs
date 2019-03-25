bac2bad95cc63ed9cb675718520e14f381b1ed71
Luke Hackett <lhackett@tcd.ie>
2019-03-25 14:20:48 +0000

    Slight updates to speech scene

--- Hololens-Nav/Assets/Scripts/API.cs
+++ Hololens-Nav/Assets/Scripts/API.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using Mapbox.Utils;
+using Mapbox.VectorTile;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,14 +10,14 @@ public class API : MonoBehaviour
     private const string URL = "https://api.mapbox.com/directions/v5/mapbox/walking/";
     private const string API_KEY = "pk.eyJ1IjoiamVuZ2VsczAzIiwiYSI6ImNqaXZ1aDVjODE0ZDQzam56dHJhc3dmNG8ifQ.2jBiS1t1o8uY4KuSbBMOtg";
 	private const string GEOCODER_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
-	private const string TEST_CURRENT_COORDS = "53.3438,6.2546";
+	private const string TEST_CURRENT_COORDS = "-6.2546,53.3438";
 	private string destination;
     public Text responseText;
 
     public void request()
     {
         //https://api.mapbox.com/directions/v5/mapbox/walking/-6.5207408,53.511152;-6.257853,53.344444?steps=true&voice_instructions=true&banner_instructions=true&voice_units=metric&waypoint_names=Home;Work&access_token=pk.eyJ1IjoiZmllbGRzYWwiLCJhIjoiY2pzbXlwNGF4MDY0ZDQ5cGVrN2MwcG45dCJ9.MYsalppgAht84dBaUjW-zw
-        Debug.Log(URL + destination + "?" + "steps=true&voice_instructions=true&banner_instructions=true&voice_units=metric" + "&access_token=" + API_KEY);
+        Debug.Log(URL + TEST_CURRENT_COORDS + ";" + destination + "?" + "geometries=polyline&steps=true&voice_instructions=true&banner_instructions=true&voice_units=metric" + "&access_token=" + API_KEY);
         WWW request = new WWW(URL + TEST_CURRENT_COORDS + ";" + destination + ".json?" + "steps=true&voice_instructions=true&banner_instructions=true&voice_units=metric" + "&access_token=" + API_KEY);
         Debug.Log("RT: Called request for coordinates " + destination);
         StartCoroutine(OnResponse(request));
@@ -38,6 +40,14 @@ public class API : MonoBehaviour
                             text += currentVoiceInstructions.announcement + "\n";
                         }
             Debug.Log(text);
+            Debug.Log("Geometry: " + route.routes[0].geometry);
+            List<Vector2d> line = PolylineUtils.Decode(route.routes[0].geometry);
+            foreach(Vector2d v in line)
+            {
+                Debug.Log("Lat : " + v.y + " and Long: " + v.x);
+            }
+
+            // TODO load map and stuff here
         }
     }
 
@@ -68,6 +78,7 @@ public class API : MonoBehaviour
 		string coords = geocodeJson.Substring(substringStart, substringEnd-substringStart);
 		Debug.Log("GC: Parsed out " + coords + " as coordinates");
 		destination = coords;
-		request();
+
+        request();
 	} 
 }
