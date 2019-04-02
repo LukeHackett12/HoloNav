c879d3d9c5dd914a05464d7bed8b2f360f3d64a5
Luke Hackett <lhackett@tcd.ie>
2019-04-02 16:04:11 +0100

    Update AppXBuild.ps1

--- AppXBuild.ps1
+++ AppXBuild.ps1
@@ -4,4 +4,4 @@
 
 cd $project/UWP; 
 nuget restore $project.sln; 
-msbuild.exe $project.sln /t:Build /p:Configuration=Release /p:Platform=x86 /verbosity:d
+msbuild.exe $project.sln /t:Build /p:Configuration=Debug /p:Platform=x86 /verbosity:d
